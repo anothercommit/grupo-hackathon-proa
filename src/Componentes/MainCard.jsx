870835ff3479a703552d8cbfa15723a777578661
Your Name <you@example.com>
2024-09-03 14:44:32 -0300

    🦎

--- src/Componentes/MainCard.jsx
+++ src/Componentes/MainCard.jsx
@@ -5,6 +5,15 @@ function MainCard({ data }) {
     return (
         <>
             <p>Calidad del aire: {calidades[data.list[0].main.aqi]}</p>
+            <p>Contamintantes:</p>
+            <ul>
+                <li>PM2.5: {data.list[0].components.pm2_5}</li>
+                <li>PM10: {data.list[0].components.pm10}</li>
+                <li>O3: {data.list[0].components.o3}</li>
+                <li>NO2: {data.list[0].components.no2}</li>
+                <li>SO2: {data.list[0].components.so2}</li>
+                <li>CO: {data.list[0].components.co}</li>
+            </ul>
         </>
     );
 }
