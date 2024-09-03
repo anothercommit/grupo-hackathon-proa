4d50a78c91700bfe557747cfe5543b5c7489f8be
Your Name <you@example.com>
2024-09-03 10:58:22 -0300

    🦎

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 //componentes
@@ -6,17 +6,21 @@ import Footer from "./Componentes/Footer";
 import Navbar from "./Componentes/Navbar";
 import Context from "./Componentes/Context";
 
-//componentes
-import Footer from "./Componentes/Footer";
-import Navbar from "./Componentes/Navbar";
-
 function App() {
   const [latitud_altitud, setLatitud_altitud] = useState([]);
+
+  const handleButton = () => {
+    axios.get(
+      "http://api.openweathermap.org/data/2.5/air_pollution?lat=50&lon=50&appid=2ca3a7c5d310204cd624d782488f7690",
+    );
+  };
+
   return (
     <>
       <Navbar />
       <Context />
       <Footer />
+      <button onClick={handleButton}>Ver calidad de aire</button>
     </>
   );
 }
